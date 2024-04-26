2627aeb08010bcdd961227d190bed08f48ce61e9
zachrd <zacharyrobertdavis@gmail.com>
2024-04-26 17:59:09 -0400

    Date and Time Changes

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -6,7 +6,7 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Memorial Arrangements</h1>
-      <p><em>Saturday, May 25th 2024</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Lyns life later in the year when COVID-19 restrictions are relaxed.</p>
+      <p><time datetime="2024-05-25 13:00-04:00"><em>Saturday, May 25th 2024 1:00PM EST</em></p></time>
       <span> 1426 E. Fort Avenue, Baltimore, MD 21230<div class="showtheway">
       <a href="https://showtheway.io/to/39.269386,-76.59413?name=1426%20E%20Fort%20Ave" target="_blank" title="Show the Way to 1426 E Fort Ave with your favorite navigation application">Show the Way</a>
       </div><script src="https://showtheway.io/w.js" async="async" type="text/javascript"></script></span>
