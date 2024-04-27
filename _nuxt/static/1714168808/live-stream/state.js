ea9353a9380461965235673450153cb7011f9318
zachrd <zachrd@users.noreply.github.com>
2024-04-27 03:43:41 +0000

    deploy: ebb7ece03b38108db5a25f5223af277ae45ce6d5

--- _nuxt/static/1714168808/live-stream/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714168808",layout:"default",error:a,serverRendered:true,routePath:"\u002Flive-stream",config:{name:"William Patrick Davis",dates:"March 17th 1938 - April 4th 2024",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
