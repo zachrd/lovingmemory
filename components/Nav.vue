91f1c9cef16f5638f96b482f2feb0205bd235338
zachrd <zacharyrobertdavis@gmail.com>
2024-04-26 23:45:11 -0400

    Fix HTML TAG BUG

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,7 +5,7 @@
           </span><span class="menu">Home</span></NuxtLink></li>
         <li><NuxtLink to="/memorial-arrangements"><span class="icon">
           <svg class="svg-inline--fa fa-map fa-w-18 fa-fw" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M0 117.66v346.32c0 11.32 11.43 19.06 21.94 14.86L160 416V32L20.12 87.95A32.006 32.006 0 0 0 0 117.66zM192 416l192 64V96L192 32v384zM554.06 33.16L416 96v384l139.88-55.95A31.996 31.996 0 0 0 576 394.34V48.02c0-11.32-11.43-19.06-21.94-14.86z"></path></svg>
-          </span><span class="menu"><span>Memorial </span>Arrangements</span></NuxtLink></li>
+          </span><span class="menu"><span>Memorial </span>Celebration of Life</span></NuxtLink></li>
         <!--  <li><NuxtLink to="/live-stream"><span class="icon">
           <svg class="svg-inline--fa fa-youtube fa-w-18 fa-fw" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path></svg>
           </span><span class="menu"><span>Service </span>Livestream</span></NuxtLink></li> -->
--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -5,8 +5,8 @@
     </div>
     <div class="section details">
       <!-- CONTENT START -->
-      <h1 class="title">Memorial Arrangements</h1>
-      <p><time datetime="2024-05-25 13:00-04:00"><em>Saturday, May 25th 2024 1:00PM EST</em></p></time>
+      <h1 class="title">Celebration of Life</h1>
+      <time datetime="2024-05-25 13:00-04:00"><em>Saturday, May 25th 2024 1:00PM EST</em></time>
       <span> 1426 E. Fort Avenue, Baltimore, MD 21230<div class="showtheway">
       <a href="https://showtheway.io/to/39.269386,-76.59413?name=1426%20E%20Fort%20Ave" target="_blank" title="Show the Way to 1426 E Fort Ave with your favorite navigation application">Show the Way</a>
       </div><script src="https://showtheway.io/w.js" async="async" type="text/javascript"></script></span>
