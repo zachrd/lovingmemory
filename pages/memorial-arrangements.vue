6dad4e41c5bfaf096b1b44b0227b764773044af5
zachrd <zacharyrobertdavis@gmail.com>
2024-05-08 21:38:25 -0400

    Memorial Details

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -6,10 +6,9 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Celebration of Life</h1>
-      <time datetime="2024-05-25 13:00-04:00"><em>Saturday, May 25th 2024 1:00PM EST</em></time>
-      <span> 1426 E. Fort Avenue, Baltimore, MD 21230<div class="showtheway">
-      <a href="https://showtheway.io/to/39.269386,-76.59413?name=1426%20E%20Fort%20Ave" target="_blank" title="Show the Way to 1426 E Fort Ave with your favorite navigation application">Show the Way</a>
-      </div><script src="https://showtheway.io/w.js" async="async" type="text/javascript"></script></span>
+      <p>When:<time datetime="2024-05-25 13:00-04:00"><em>Saturday, May 25th 2024 1:00PM EST</em></time></p>
+      <p>Where:<span>1426 E. Fort Avenue, Baltimore, MD 21230</span></p>
+      <p>Parking:</br> Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. PLease observe all signs to avoid ticketing.</p>
       <!-- CONTENT END -->
     </div>
   </div>
