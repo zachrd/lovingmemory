d97a738f0c061c823b185a150843aaafd87f2fd0
zachrd <zacharyrobertdavis@gmail.com>
2024-05-08 21:49:13 -0400

    Donations page

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -6,11 +6,14 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Charitable Donations</h1>
-      <p>In lieu we would like to suggest people make a donation to one of the following charities</p>
+      <p>In lieu of flowers we would like to suggest considering making a donation to one of the following organizations.</p>
       <h2>St. Jude Children's Research Hospital</h2>
-      <p>Patrick was a regular supporter of <em>St. Jude's</em> and we believe he would appreciate everyone continuing to support the hospital. 
+      <p>Patrick was a regular supporter of St. Jude's and we believe he would appreciate everyone continuing to support the hospital. 
       </p>
       <a class="button" target="_blank" rel=”noopener” href="https://www.stjude.org/give.html">Donate to St. Jude Children's Research Hospital</a>
+      <h2>Marine Corps League, Baltimore Detachment #565</h2>
+      <p>Patrick was a life member of the Marine Corps League, serving as Paymaster for several years. To donate checks to be made out to Marine Corps League, Baltimore Detachment #565, and be sent to 1426 E. Fort Avenue, Baltimore, MD 21230.</p>
+      <a class="button" target="_blank" rel=”noopener” href="http://mclbaltimore.org/">Visit MCL Baltimore for More infomation.</a>
       <!-- CONTENT END -->
     </div>
   </div>
