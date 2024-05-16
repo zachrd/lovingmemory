1d2c447384d6d03871e524f5886410e3898c6472
zachrd <zacharyrobertdavis@gmail.com>
2024-05-15 22:19:26 -0400

    change address schema

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -7,7 +7,13 @@
       <!-- CONTENT START -->
       <h1 class="title">Celebration of Life</h1>
       <p><strong>When:</strong>&emsp;<time datetime="2024-05-25 13:00-04:00"><strong>Saturday, May 25th 2024 1:00PM EST</strong></time></p>
-      <p><strong>Where:</strong>&emsp;1426 E. Fort Avenue, Baltimore, MD 21230</p>
+      <p><strong>Where:</strong>&emsp;<div vocab="http://schema.org/" typeof="PostalAddress">
+    <span property="streetAddress">1426 East Fort Avenue</span>
+    <span property="addressLocality">Baltimore</span>,
+    <span property="addressRegion">MD</span>
+    <span property="postalCode">21230</span>
+   </div>
+   </p>
       <p><strong>Parking:</strong>&emsp;Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. Please observe all signs to avoid ticketing.</p>
       <!-- CONTENT END -->
     </div>
