ab75e14e3eff70b34fb383d5caa5bb8d7e50a14b
zachrd <zacharyrobertdavis@gmail.com>
2024-05-16 01:04:04 -0400

    fix embed

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -10,9 +10,8 @@
       <p><strong>Where:</strong>&emsp;<div vocab="http://schema.org/" typeof="PostalAddress">&nbsp;
       <span property="streetAddress">1426 East Fort Avenue</span>&nbsp;<span property="addressLocality">Baltimore</span>,&nbsp;<span property="addressRegion">MD</span>&nbsp;<span property="postalCode">21230</span></div></p>
       <p><strong>Parking:</strong>&emsp;Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. Please observe all signs to avoid ticketing.</p>
-
       <p><strong>RSVP:</strong></p>
-      <iframe src="https://gath.io/WIkCsdTDFMACrbxSug4pf" title="Celebration of Life for William Patrick Davis &middot; Gathio">
+      <iframe src="https://gath.io/WIkCsdTDFMACrbxSug4pf" width="90%" title="Celebration of Life for William Patrick Davis &middot; Gathio">
       </iframe>
 
       <!-- CONTENT END -->
