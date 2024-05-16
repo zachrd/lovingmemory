7932650570f184bf13f20a6c1f32ff9401182847
zachrd <zacharyrobertdavis@gmail.com>
2024-05-16 00:56:20 -0400

    embed gathio site

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -8,8 +8,13 @@
       <h1 class="title">Celebration of Life</h1>
       <p><strong>When:</strong>&emsp;<time datetime="2024-05-25 13:00-04:00"><strong>Saturday, May 25th 2024 1:00PM EST</strong></time></p>
       <p><strong>Where:</strong>&emsp;<div vocab="http://schema.org/" typeof="PostalAddress">&nbsp;
-    <span property="streetAddress">1426 East Fort Avenue</span>&nbsp;<span property="addressLocality">Baltimore</span>,&nbsp;<span property="addressRegion">MD</span>&nbsp;<span property="postalCode">21230</span></div></p>
+      <span property="streetAddress">1426 East Fort Avenue</span>&nbsp;<span property="addressLocality">Baltimore</span>,&nbsp;<span property="addressRegion">MD</span>&nbsp;<span property="postalCode">21230</span></div></p>
       <p><strong>Parking:</strong>&emsp;Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. Please observe all signs to avoid ticketing.</p>
+
+      <p><strong>RSVP:</strong></p>
+      <iframe src="https://gath.io/WIkCsdTDFMACrbxSug4pf" title="Celebration of Life for William Patrick Davis &middot; Gathio">
+      </iframe>
+
       <!-- CONTENT END -->
     </div>
   </div>
