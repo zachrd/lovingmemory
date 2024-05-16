576607150fe3822a8130ad53f3cc393e609f164b
zachrd <zacharyrobertdavis@gmail.com>
2024-05-15 22:09:00 -0400

    change spacing

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -6,9 +6,9 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Celebration of Life</h1>
-      <p><strong>When:</strong> <time datetime="2024-05-25 13:00-04:00"><strong>Saturday, May 25th 2024 1:00PM EST</strong></time></p>
-      <p><strong>Where:</strong> <span>1426 E. Fort Avenue, Baltimore, MD 21230</span></p>
-      <p><strong>Parking:</strong> Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. Please observe all signs to avoid ticketing.</p>
+      <p><strong>When:</strong>&emsp;<time datetime="2024-05-25 13:00-04:00"><strong>Saturday, May 25th 2024 1:00PM EST</strong></time></p>
+      <p><strong>Where:</strong>&emsp;1426 E. Fort Avenue, Baltimore, MD 21230</p>
+      <p><strong>Parking:</strong>&emsp;Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. Please observe all signs to avoid ticketing.</p>
       <!-- CONTENT END -->
     </div>
   </div>
