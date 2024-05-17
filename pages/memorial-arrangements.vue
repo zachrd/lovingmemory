9f071da15efcc360db44351234b75ff119627b41
zachrd <zacharyrobertdavis@gmail.com>
2024-05-17 01:12:45 -0400

    add final images an hopefully fix formatting so mom can calm down.

--- pages/memorial-arrangements.vue
+++ pages/memorial-arrangements.vue
@@ -7,8 +7,7 @@
       <!-- CONTENT START -->
       <h1 class="title">Celebration of Life</h1>
       <p><strong>When:</strong>&emsp;<time datetime="2024-05-25 13:00-04:00"><strong>Saturday, May 25th 2024 1:00PM EST</strong></time></p>
-      <p><strong>Where:</strong>&emsp;<div vocab="http://schema.org/" typeof="PostalAddress">&nbsp;
-      <span property="streetAddress">1426 East Fort Avenue</span>&nbsp;<span property="addressLocality">Baltimore</span>,&nbsp;<span property="addressRegion">MD</span>&nbsp;<span property="postalCode">21230</span></div></p>
+      <p><strong>Where:</strong>&emsp;1426 East Fort Avenue&nbsp;Baltimore,&nbsp;MD&nbsp;21230</p>
       <p><strong>Parking:</strong>&emsp;Parking is available across the street at Francis Scott Key Elementary/Middle School as well as on the street. Please observe all signs to avoid ticketing.</p>
       </br>
       <a class="button" target="_blank" rel=”noopener” href="https://gath.io/WIkCsdTDFMACrbxSug4pf">PLEASE RSVP HERE</a>
