1b2f41901023a58623f7c5f71e9ed5b591b0b982
zachrd <zachrd@users.noreply.github.com>
2024-05-25 07:14:43 +0000

    deploy: bc2b044e5c79e6ae0760c1aee8d9d4257d9f358b

---
+++ _nuxt/static/1716621276/charity-donation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1716621276",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcharity-donation",config:{name:"William Patrick Davis",dates:"March 17th 1938 - April 4th 2024",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
